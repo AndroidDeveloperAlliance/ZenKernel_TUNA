abe5e9ffc9b15461d2118ddd556fb304f8132a9f
Brandon Berhent <bbedward@androiddeveloperalliance.org>
2012-11-07 09:28:57 -0500

    Tweak lowmemorykiller, rubix's setting: 2048,3072,6144,15360,17920,20480

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -44,11 +44,20 @@ static int lowmem_adj[6] = {
 	12,
 };
 static int lowmem_adj_size = 4;
+/* Default minfree:
+        3 * 512,
+        2 * 1024,
+        4 * 1024,
+        16 * 1024,
+*/
+
 static size_t lowmem_minfree[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	16 * 1024,	/* 64MB */
+	2048,
+	3072,
+	6144,
+	15360,
+	17920,
+	20480,
 };
 static int lowmem_minfree_size = 4;
 

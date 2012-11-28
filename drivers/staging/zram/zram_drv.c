30b682625072a9e70326db951b9f65fd3854dd57
Brandon Berhent <bbedward@androiddeveloperalliance.org>
2012-11-28 08:14:49 -0500

    Revert "Add zram for android optimization"
    This reverts commit 5fec809dcf383a1b0689a112e4b8cb73d69bca80.
    
    Conflicts:
    	drivers/staging/zram/Kconfig
    	drivers/staging/zram/zram_drv.c
    	drivers/staging/zram/zram_drv.h
    	drivers/staging/zram/zram_sysfs.c

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -31,10 +31,6 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/vmalloc.h>
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-#include <linux/swap.h>
-#endif /* CONFIG_ZRAM_FOR_ANDROID */
-
 
 #include "zram_drv.h"
 
@@ -84,7 +80,7 @@ snappy_decompress_(
 
 /* Globals */
 static int zram_major;
-struct zram *zram_devices;
+struct zram *devices;
 
 /* Module params (documentation at end) */
 unsigned int zram_num_devices;
@@ -162,22 +158,6 @@ static void zram_set_disksize(struct zram *zram, u64 size_bytes)
 	zram->disksize = size_bytes;
 	set_capacity(zram->disk, size_bytes >> SECTOR_SHIFT);
 }
-
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-/*
- * Swap header (1st page of swap device) contains information
- * about a swap file/partition. Prepare such a header for the
- * given ramzswap device so that swapon can identify it as a
- * swap partition.
- */
-static void setup_swap_header(struct zram *zram, union swap_header *s)
-{
-	s->info.version = 1;
-	s->info.last_page = (zram->disksize >> PAGE_SHIFT) - 1;
-	s->info.nr_badpages = 0;
-	memcpy(s->magic.magic, "SWAPSPACE2", 10);
-}
-#endif /* CONFIG_ZRAM_FOR_ANDROID */
 
 static void zram_free_page(struct zram *zram, size_t index)
 {
@@ -685,10 +665,6 @@ int zram_init_device(struct zram *zram)
 {
 	int ret;
 	size_t num_pages;
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-	struct page *page;
-	union swap_header *swap_header;
-#endif /* CONFIG_ZRAM_FOR_ANDROID */
 
 	down_write(&zram->init_lock);
 
@@ -723,6 +699,7 @@ int zram_init_device(struct zram *zram)
 		goto fail_no_table;
 	}
 
+<<<<<<< HEAD
 #ifdef CONFIG_ZRAM_FOR_ANDROID
 	page = alloc_page(__GFP_ZERO);
 	if (!page) {
@@ -736,6 +713,10 @@ int zram_init_device(struct zram *zram)
 	setup_swap_header(zram, swap_header);
 	kunmap(page);
 #endif /* CONFIG_ZRAM_FOR_ANDROID */
+=======
+	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
+
+>>>>>>> parent of 5fec809... Add zram for android optimization
 	/* zram devices sort of resembles non-rotational disks */
 	queue_flag_set_unlocked(QUEUE_FLAG_NONROT, zram->disk->queue);
 
@@ -879,15 +860,26 @@ static int __init zram_init(void)
 	}
 
 	/* Allocate the device array and initialize each one */
+<<<<<<< HEAD
 	pr_info("Creating %u devices ...\n", zram_num_devices);
 	zram_devices = kzalloc(zram_num_devices * sizeof(struct zram), GFP_KERNEL);
 	if (!zram_devices) {
+=======
+	pr_info("Creating %u devices ...\n", num_devices);
+	devices = kzalloc(num_devices * sizeof(struct zram), GFP_KERNEL);
+	if (!devices) {
+>>>>>>> parent of 5fec809... Add zram for android optimization
 		ret = -ENOMEM;
 		goto unregister;
 	}
 
+<<<<<<< HEAD
 	for (dev_id = 0; dev_id < zram_num_devices; dev_id++) {
 		ret = create_device(&zram_devices[dev_id], dev_id);
+=======
+	for (dev_id = 0; dev_id < num_devices; dev_id++) {
+		ret = create_device(&devices[dev_id], dev_id);
+>>>>>>> parent of 5fec809... Add zram for android optimization
 		if (ret)
 			goto free_devices;
 	}
@@ -896,8 +888,8 @@ static int __init zram_init(void)
 
 free_devices:
 	while (dev_id)
-		destroy_device(&zram_devices[--dev_id]);
-	kfree(zram_devices);
+		destroy_device(&devices[--dev_id]);
+	kfree(devices);
 unregister:
 	unregister_blkdev(zram_major, "zram");
 out:
@@ -909,8 +901,13 @@ static void __exit zram_exit(void)
 	int i;
 	struct zram *zram;
 
+<<<<<<< HEAD
 	for (i = 0; i < zram_num_devices; i++) {
 		zram = &zram_devices[i];
+=======
+	for (i = 0; i < num_devices; i++) {
+		zram = &devices[i];
+>>>>>>> parent of 5fec809... Add zram for android optimization
 
 		destroy_device(zram);
 		if (zram->init_done)
@@ -919,7 +916,7 @@ static void __exit zram_exit(void)
 
 	unregister_blkdev(zram_major, "zram");
 
-	kfree(zram_devices);
+	kfree(devices);
 	pr_debug("Cleanup done!\n");
 }
 

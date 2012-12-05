0e30877b3a2b30daf24ad46bf9fa60be92ee3716
Brandon Berhent <bbedward@androiddeveloperalliance.org>
2012-12-05 16:18:57 -0500

    Add intellidemandX governor, intellidemand with suspend/wake+hotplug code

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -367,6 +367,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_intellidemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemandx)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEQ)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lulzactive)

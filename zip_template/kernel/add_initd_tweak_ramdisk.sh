1c4a70e7c5b1d2aaa414863ca5e5c6bd34d0bf94
Brandon Berhent <bbedward@androiddeveloperalliance.org>
2012-12-05 15:32:46 -0500

    Add zip_template and some stuff to .gitignore

---
+++ zip_template/kernel/add_initd_tweak_ramdisk.sh
@@ -0,0 +1,40 @@
+#!/tmp/busybox sh
+cd /tmp
+mkdir ramdisk
+cd ramdisk
+/tmp/busybox gzip -dc ../boot.img-ramdisk.gz | /tmp/busybox cpio -i
+# If init.rc contains 'init.d' it probably already runs init scripts from there
+if [ -z `/tmp/busybox grep init.d init.rc` ]; then
+	echo '' >> init.rc
+	echo 'service run_parts /system/sbin/bb/busybox run-parts /system/etc/init.d' >> init.rc
+	echo '    class main' >> init.rc
+	echo '    oneshot' >> init.rc
+fi
+# Ensure min/max frequency scaling perms are properly set in the ramdisk
+if [ -z `/tmp/busybox grep 'chown root system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq' init.rc` ]; then
+	/tmp/busybox sed -i '/chown root radio \/proc\/cmdline/ a\    chown root system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq' init.rc
+fi
+if [ -z `/tmp/busybox grep 'chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq' init.rc` ]; then
+	/tmp/busybox sed -i '/chown root system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/ a\    chmod 0664 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq' init.rc
+fi
+if [ -z `/tmp/busybox grep 'chown root system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' init.rc` ]; then
+	/tmp/busybox sed -i '/chmod 0664 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/ a\    chown root system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq' init.rc
+fi
+if [ -z `/tmp/busybox grep 'chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq' init.rc` ]; then
+	/tmp/busybox sed -i '/chown root system \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/ a\    chmod 0664 \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq' init.rc
+fi
+# Cleanup existing init.rc of things we don't want it to set
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_max_freq/d' init.rc
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/d' init.rc
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/screen_off_max_freq/d' init.rc
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor/d' init.rc
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_max_freq/d' init.rc
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_min_freq/d' init.rc
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/screen_off_max_freq/d' init.rc
+/tmp/busybox sed -i '/write \/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_governor/d' init.rc
+# Insert fstab with additional mount options
+cp ../fstab.tuna fstab.tuna
+rm ../boot.img-ramdisk.gz
+/tmp/busybox sh -c "/tmp/busybox find . | /tmp/busybox cpio -o -H newc | /tmp/busybox gzip > ../boot.img-ramdisk.gz"
+cd ..
+
